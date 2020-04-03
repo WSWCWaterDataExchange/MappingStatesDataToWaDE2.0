0a46d0fe662ab6ee233d78b004c3bf2feb8c9a96
Ryan James <rjames@wswc.utah.gov>
2020-04-03 16:44:51 -0600

    Update to Oregon data.
    Update to Oregon data 04/03/2020.

---
+++ Oregon/WaterAllocation/1_OR_Methods.py
@@ -0,0 +1,79 @@
+#Date Created: 04/03/2020
+#Purpose: To extract OR methods use information and population dataframe for WaDE_QA 2.0.
+#Notes:
+
+
+# Needed Libraries
+############################################################################
+import pandas as pd
+import numpy as np
+import os
+
+
+# Inputs
+############################################################################
+print("Reading inputs...")
+workingDir = "C:/Users/rjame/Documents/WSWC Documents/MappingStatesDataToWaDE2.0/Oregon/WaterAllocation"
+os.chdir(workingDir)
+
+#WaDE columns
+columnslist = [
+    "MethodUUID",
+    "ApplicableResourceTypeCV",
+    "DataConfidenceValue",
+    "DataCoverageValue",
+    "DataQualityValueCV",
+    "MethodDescription",
+    "MethodName",
+    "MethodNEMILink",
+    "MethodTypeCV"]
+
+
+# Creating output dataframe (outdf)
+############################################################################
+print("Populating dataframe...")
+
+outdf = pd.DataFrame(columns=columnslist)
+
+outdf.MethodUUID = "OWRD_Water Rights"
+
+outdf.ApplicableResourceTypeCV = "Surface/Ground/Storage"
+
+outdf.DataConfidenceValue = ""
+
+outdf.DataQualityValueCV = ""
+
+outdf.DataCoverageValue = ""
+
+outdf.MethodDescription = "Water Rights"
+
+outdf.MethodName = "Oregon Water Rights"
+
+outdf.MethodNEMILink = "https://www.oregon.gov/OWRD/access_Data/Pages/Data.aspx"
+
+outdf.MethodTypeCV = "Adjudicated"
+
+# Check required fields are not null
+############################################################################
+print("Check required is not null...")
+#Check all 'required' (not NA) columns have value (not empty). Replace blank strings by NaN, if there are any
+outdf = outdf.replace('', np.nan)
+outdf_nullMand = outdf.loc[(outdf["MethodUUID"].isnull()) | (outdf["MethodUUID"] == '') |
+                           (outdf["MethodName"].isnull()) | (outdf["MethodName"] == '') |
+                           (outdf["MethodDescription"].isnull()) | (outdf["MethodDescription"] == '') |
+                           (outdf["ApplicableResourceTypeCV"].isnull()) | (outdf["ApplicableResourceTypeCV"] == '') |
+                           (outdf["MethodTypeCV"].isnull()) | (outdf["MethodTypeCV"] == '')]
+
+
+# Export to new csv
+############################################################################
+print("Exporting dataframe to csv...")
+
+outdf.to_csv('ProcessedInputData/methods.csv', index=False)
+
+#Report missing values if need be to separate csv
+if(len(outdf_nullMand.index) > 0):
+    outdf_nullMand.to_csv('ProcessedInputData/methods_missing.csv')  # index=False,
+
+
+print("Done.")
---
+++ Oregon/WaterAllocation/5_OR_Sites.py
@@ -0,0 +1,329 @@
+#Date Created: 04/03/2020
+#Purpose: To extract OR site use information and population dataframe for WaDEQA 2.0.
+#Notes: 1) Have to convert from epsg:2992 - to - epsg:4326 in order for lat and long to work in WaDE 2.0.
+#       2) Transformer from pyproj is incredibly inefficient.  Did discover trick of preloading necessary coord systems.
+#       3) Large files, better to use the '.apply() lambda row' method with a few custom functions, rather than a 'for' loop.
+
+
+# Needed Libraries
+############################################################################
+import pandas as pd
+import numpy as np
+import os
+from pyproj import Transformer, transform
+
+transformer = Transformer.from_proj(2992, 4326)  # A trick to drastically optimize the Transformer of pyproj.
+# Oregon projection = EPSG:2992. WGS84 projection used by WaDE 2.0 = epsg:4326.
+
+# Inputs
+############################################################################
+print("Reading input csv...")
+workingDir = "C:/Users/rjame/Documents/WSWC Documents/MappingStatesDataToWaDE2.0/Oregon/WaterAllocation"
+os.chdir(workingDir)
+fileInput = "RawinputData/P_OregonMaster.csv"
+df = pd.read_csv(fileInput)
+
+columnslist = [
+    "SiteUUID",
+    "CoordinateAccuracy",
+    "CoordinateMethodCV",
+    "County",
+    "EPSGCodeCV",
+    "Geometry",
+    "GNISCodeCV",
+    "HUC12",
+    "HUC8",
+    "Latitude",
+    "Longitude",
+    "NHDNetworkStatusCV",
+    "NHDProductCV",
+    "SiteName",
+    "SiteNativeID",
+    "SitePoint",
+    "SiteTypeCV",
+    "StateCV",
+    "USGSSiteID"]
+
+# Custom Functions
+############################################################################
+
+# For converting projection latitude.
+def assignLat(colrowValueLat, colrowValueLong):
+    lat, long = transformer.transform(colrowValueLat, colrowValueLong)
+    return lat
+
+# For converting projection longitude.
+def assignLong(colrowValueLat, colrowValueLong):
+    lat, long = transformer.transform(colrowValueLat, colrowValueLong)
+    return long
+
+def assignSiteName(colrowValueA, colrowValueB):
+    if (colrowValueA == '' and colrowValueB == '') or (pd.isnull(colrowValueA) and pd.isnull(colrowValueB)):
+        outList = "Unspecified"
+    else:
+        A = str(colrowValueA).strip()
+        B = str(colrowValueB).strip()
+        outList = A + "_" + B
+    return outList
+
+# For creating SiteTypeCV
+STCVDict = {
+"LK" : "lake",
+"DR" : "drain",
+"SP" : "spring",
+"ST" : "stream",
+"SL" : "slough",
+"WW" : "waste water",
+"WE" : "well",
+"WR" : "winter runoff",
+"SM" : "sump",
+"PD" : "pond",
+"RS" : "reservoir",
+"DT" : "ditch",
+"SE" : "sewage effluent",
+"CN" : "canal"}
+def assignSiteTypeCV(colrowValue):
+    if colrowValue == '' or pd.isnull(colrowValue):
+        outList = 'Unknown'
+    else:
+        String1 = colrowValue.strip()  # remove whitespace chars
+        try:
+            outList = STCVDict[String1]
+        except:
+            outList = 'Unknown'
+    return outList
+
+# For creating SiteUUID
+def assignSiteUUID(colrowValue):
+    string1 = str(colrowValue)
+    outstring = "OR_" + string1
+    return outstring
+
+
+# Creating output dataframe (outdf)
+############################################################################
+print("Populating dataframe...")
+outdf = pd.DataFrame(columns=columnslist, index=df.index)
+
+print("CoordinateAccuracy")
+outdf.CoordinateAccuracy = ''
+
+print("againCoordinateMethodCV")  # Hardcoded
+outdf.CoordinateMethodCV = 'Unspecified'
+
+print("County")  # Hardcoded
+#outdf.County = ""
+
+print("EPSGCodeCV")  # Hardcoded
+outdf.EPSGCodeCV = 'EPSG:4326'
+
+print("Geometry") # Hardcoded
+outdf.Geometry = ""
+
+print("GNISCodeCV")  # Hardcoded
+outdf.GNISCodeCV = ""
+
+print("HUC12")  # Hardcoded
+outdf.HUC12 = ""
+
+print("HUC8")  # Hardcoded
+outdf.HUC8 = ""
+
+print("Latitude")
+outdf['Latitude'] = df.apply(lambda row: assignLat(row['POINT_X'], row['POINT_Y']), axis=1)
+
+print("Longitude")
+outdf['Longitude'] = df.apply(lambda row: assignLong(row['POINT_X'], row['POINT_Y']), axis=1)
+
+print("NHDNetworkStatusCV")  # Hardcoded
+outdf.NHDNetworkStatusCV = ""
+
+print("NHDProductCV")  # Hardcoded
+outdf.NHDProductCV = ""
+
+print("SiteName")  # Hardcoded
+outdf['SiteName'] = df.apply(lambda row: assignSiteName(row['snp_id'], row['pod_nbr']), axis=1)
+
+print("SiteNativeID")
+outdf['SiteNativeID'] = df['pod_location_id'].astype(str) # Native dbtype is float. Need to return this value as a string
+
+print("SitePoint")  # Hardcoded
+outdf.SitePoint = ""
+
+print("SiteTypeCV")
+outdf['SiteTypeCV'] = df.apply(lambda row: assignSiteTypeCV(row['source_type']), axis=1)
+
+print("StateCV")  # Hardcoded
+outdf.StateCV = "OR"
+
+print("USGSSiteID")  # Hardcoded
+outdf.USGSSiteID = ""
+
+print("Resetting Index")  # Hardcoded
+outdf.reset_index()
+
+#####################################
+# Dropping duplicate
+# filter the whole table based on a unique combination of SiteNativeID, SiteName, SiteTypeCV, Longitude & Latitude
+outdf = outdf.drop_duplicates(subset=['SiteNativeID', 'SiteName', 'SiteTypeCV', 'Longitude', 'Latitude'])
+outdf = outdf.reset_index(drop=True)
+outdfpurge = outdf.loc[(outdf['Longitude'].isnull()) | (outdf['Latitude'].isnull())]
+######################################
+
+print("SiteUUID") # has to be one of the last.
+dftemp = pd.DataFrame(index=outdf.index)
+dftemp["Count"] = range(1, len(dftemp.index) + 1)
+outdf['SiteUUID'] = dftemp.apply(lambda row: assignSiteUUID(row['Count']), axis=1)
+
+
+#Error Checking each Field
+############################################################################
+print("Error checking each field. Purging bad inputs.")
+dfpurge = pd.DataFrame(columns=columnslist)  # purge DataFrame
+dfpurge = dfpurge.assign(ReasonRemoved='')
+
+# SiteUUID_nvarchar(200)_
+mask = outdf.loc[ (outdf["SiteUUID"].isnull()) | (outdf["SiteUUID"] == '') | (outdf['SiteUUID'].str.len() > 200) ].assign(ReasonRemoved='Bad SiteUUID').reset_index()
+if len(mask.index) > 0:
+    dfpurge = dfpurge.append(mask)  # Append to purge DataFrame
+    dropIndex = outdf.loc[ (outdf["SiteUUID"].isnull()) | (outdf["SiteUUID"] == '') | (outdf['SiteUUID'].str.len() > 200) ].index
+    outdf = outdf.drop(dropIndex)
+    outdf = outdf.reset_index(drop=True)
+
+# CoordinateAccuracy_nvarchar(255)_Yes
+mask = outdf.loc[ outdf["CoordinateAccuracy"].str.len() > 255 ].assign(ReasonRemoved='Bad CoordinateAccuracy').reset_index()
+if len(mask.index) > 0:
+    dfpurge = dfpurge.append(mask)
+    dropIndex = outdf.loc[ outdf["CoordinateAccuracy"].str.len() > 255 ].index
+    outdf = outdf.drop(dropIndex)
+    outdf = outdf.reset_index(drop=True)
+
+# CoordinateMethodCV_nvarchar(100)_-
+mask = outdf.loc[ (outdf["CoordinateMethodCV"].isnull()) | (outdf["CoordinateMethodCV"] == '') | (outdf['CoordinateMethodCV'].str.len() > 100) ].assign(ReasonRemoved='Bad CoordinateMethodCV').reset_index()
+if len(mask.index) > 0:
+    dfpurge = dfpurge.append(mask)  # Append to purge DataFrame
+    dropIndex = outdf.loc[ (outdf["CoordinateMethodCV"].isnull()) | (outdf["CoordinateMethodCV"] == '') | (outdf['CoordinateMethodCV'].str.len() > 100) ].index
+    outdf = outdf.drop(dropIndex)
+    outdf = outdf.reset_index(drop=True)
+
+# County_nvarchar(20)_Yes
+mask = outdf.loc[ outdf["County"].str.len() > 20 ].assign(ReasonRemoved='Bad County').reset_index()
+if len(mask.index) > 0:
+    dfpurge = dfpurge.append(mask)
+    dropIndex = outdf.loc[ outdf["County"].str.len() > 20 ].index
+    outdf = outdf.drop(dropIndex)
+    outdf = outdf.reset_index(drop=True)
+
+# EPSGCodeCV_nvarchar(50)_-
+mask = outdf.loc[ (outdf["EPSGCodeCV"].isnull()) | (outdf["EPSGCodeCV"] == '') | (outdf['EPSGCodeCV'].str.len() > 50) ].assign(ReasonRemoved='Bad EPSGCodeCV').reset_index()
+if len(mask.index) > 0:
+    dfpurge = dfpurge.append(mask)  # Append to purge DataFrame
+    dropIndex = outdf.loc[ (outdf["EPSGCodeCV"].isnull()) | (outdf["EPSGCodeCV"] == '') | (outdf['EPSGCodeCV'].str.len() > 50) ].index
+    outdf = outdf.drop(dropIndex)
+    outdf = outdf.reset_index(drop=True)
+
+# Geometry_geometry_Yes
+# ???? How to check for geometry datatype
+
+# GNISCodeCV_nvarchar(250)_Yes
+mask = outdf.loc[ outdf["GNISCodeCV"].str.len() > 250 ].assign(ReasonRemoved='Bad GNISCodeCV').reset_index()
+if len(mask.index) > 0:
+    dfpurge = dfpurge.append(mask)
+    dropIndex = outdf.loc[ outdf["GNISCodeCV"].str.len() > 250 ].index
+    outdf = outdf.drop(dropIndex)
+    outdf = outdf.reset_index(drop=True)
+
+# HUC12_nvarchar(20)_Yes
+mask = outdf.loc[ outdf["HUC12"].str.len() > 20 ].assign(ReasonRemoved='Bad HUC12').reset_index()
+if len(mask.index) > 0:
+    dfpurge = dfpurge.append(mask)
+    dropIndex = outdf.loc[ outdf["HUC12"].str.len() > 20 ].index
+    outdf = outdf.drop(dropIndex)
+    outdf = outdf.reset_index(drop=True)
+
+# HUC8_nvarchar(20)_Yes
+mask = outdf.loc[ outdf["HUC8"].str.len() > 20 ].assign(ReasonRemoved='Bad HUC8').reset_index()
+if len(mask.index) > 0:
+    dfpurge = dfpurge.append(mask)
+    dropIndex = outdf.loc[ outdf["HUC8"].str.len() > 20 ].index
+    outdf = outdf.drop(dropIndex)
+    outdf = outdf.reset_index(drop=True)
+
+# Latitude_float_Yes
+# ???? Don't we consider null lat values bad?
+
+# Longitude_float_Yes
+# ???? Don't we consider null long values bad?
+
+# NHDNetworkStatusCV_nvarchar(50)_Yes
+mask = outdf.loc[ outdf["NHDNetworkStatusCV"].str.len() > 50 ].assign(ReasonRemoved='Bad NHDNetworkStatusCV').reset_index()
+if len(mask.index) > 0:
+    dfpurge = dfpurge.append(mask)
+    dropIndex = outdf.loc[ outdf["NHDNetworkStatusCV"].str.len() > 50 ].index
+    outdf = outdf.drop(dropIndex)
+    outdf = outdf.reset_index(drop=True)
+
+# NHDProductCV_nvarchar(50)_Yes
+mask = outdf.loc[ outdf["NHDProductCV"].str.len() > 50 ].assign(ReasonRemoved='Bad NHDProductCV').reset_index()
+if len(mask.index) > 0:
+    dfpurge = dfpurge.append(mask)
+    dropIndex = outdf.loc[ outdf["NHDProductCV"].str.len() > 50 ].index
+    outdf = outdf.drop(dropIndex)
+    outdf = outdf.reset_index(drop=True)
+
+# SiteName_nvarchar(500)_
+mask = outdf.loc[ (outdf["SiteName"].isnull()) | (outdf["SiteName"] == '') | (outdf['SiteName'].str.len() > 500) ].assign(ReasonRemoved='Bad SiteName').reset_index()
+if len(mask.index) > 0:
+    dfpurge = dfpurge.append(mask)  # Append to purge DataFrame
+    dropIndex = outdf.loc[ (outdf["SiteName"].isnull()) | (outdf["SiteName"] == '') | (outdf['SiteName'].str.len() > 500) ].index
+    outdf = outdf.drop(dropIndex)
+    outdf = outdf.reset_index(drop=True)
+
+# SiteNativeID_nvarchar(50)_Yes
+mask = outdf.loc[ outdf["SiteNativeID"].str.len() > 50 ].assign(ReasonRemoved='Bad SiteNativeID').reset_index()
+if len(mask.index) > 0:
+    dfpurge = dfpurge.append(mask)
+    dropIndex = outdf.loc[ outdf["SiteNativeID"].str.len() > 50 ].index
+    outdf = outdf.drop(dropIndex)
+    outdf = outdf.reset_index(drop=True)
+
+# SitePoint_geometry_Yes
+# ???? How to check for geometry datatype
+
+# SiteTypeCV_nvarchar(100)_Yes
+mask = outdf.loc[ outdf["SiteTypeCV"].str.len() > 100 ].assign(ReasonRemoved='Bad SiteTypeCV').reset_index()
+if len(mask.index) > 0:
+    dfpurge = dfpurge.append(mask)
+    dropIndex = outdf.loc[ outdf["SiteTypeCV"].str.len() > 100 ].index
+    outdf = outdf.drop(dropIndex)
+    outdf = outdf.reset_index(drop=True)
+
+# StateCV_nvarchar(2)_Yes
+mask = outdf.loc[ outdf["StateCV"].str.len() > 2 ].assign(ReasonRemoved='Bad StateCV').reset_index()
+if len(mask.index) > 0:
+    dfpurge = dfpurge.append(mask)
+    dropIndex = outdf.loc[ outdf["StateCV"].str.len() > 2 ].index
+    outdf = outdf.drop(dropIndex)
+    outdf = outdf.reset_index(drop=True)
+
+# USGSSiteID_nvarchar(250)_Yes
+mask = outdf.loc[ outdf["USGSSiteID"].str.len() > 250 ].assign(ReasonRemoved='Bad USGSSiteID').reset_index()
+if len(mask.index) > 0:
+    dfpurge = dfpurge.append(mask)
+    dropIndex = outdf.loc[ outdf["USGSSiteID"].str.len() > 250 ].index
+    outdf = outdf.drop(dropIndex)
+    outdf = outdf.reset_index(drop=True)
+
+
+# Export to new csv
+############################################################################
+print("Exporting dataframe outdf to csv...")
+# The working output DataFrame for WaDE 2.0 input.
+outdf.to_csv('ProcessedInputData/sites.csv', index=False)
+
+# Report purged values.
+if(len(dfpurge.index) > 0):
+    dfpurge.to_csv('ProcessedInputData/sites_missing.csv')  # index=False,
+
+print("Done.")
+
