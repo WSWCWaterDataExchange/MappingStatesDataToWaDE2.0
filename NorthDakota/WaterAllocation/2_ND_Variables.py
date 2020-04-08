3bf94a121936bc3a3685981faaa0677694d929cc
Ryan James <rjames@wswc.utah.gov>
2020-04-08 13:43:43 -0600

    North Dakota data update 04/02/2020.
    North Dakota data update 04/02/2020.  To match latest WaDE QA structure.

---
+++ NorthDakota/WaterAllocation/2_ND_Variables.py
@@ -0,0 +1,88 @@
+#Date Created: 04/08/2020
+#Purpose: To create ND variable use information and population dataframe for WaDE_QA 2.0.
+#Notes: 1) Single row of entries, inpVals, for Variable Table.
+
+
+# Needed Libraries
+############################################################################
+import pandas as pd
+import numpy as np
+import os
+
+
+# Inputs
+############################################################################
+print("Reading inputs...")
+workingDir = "C:/Users/rjame/Documents/WSWC Documents/MappingStatesDataToWaDE2.0/NorthDakota/WaterAllocation"
+os.chdir(workingDir)
+
+#WaDE columns
+columnslist = [
+    "VariableSpecificUUID",
+    "AggregationInterval",
+    "AggregationIntervalUnitCV",
+    "AggregationStatisticCV",
+    "AmountUnitCV",
+    "MaximumAmountUnitCV",
+    "ReportYearStartMonth",
+    "ReportYearTypeCV",
+    "VariableCV",
+    "VariableSpecificCV"]
+
+
+# Creating output dataframe (outdf)
+############################################################################
+print("Populating dataframe...")
+
+outdf = pd.DataFrame(columns=columnslist)
+outdf = outdf.append(pd.Series(), ignore_index = True)  # This approach requires a blank row to be appended into the outbound dataframe.
+
+outdf.VariableSpecificUUID = "NDSWC_Allocation All"
+
+outdf.AggregationInterval = "1"
+
+outdf.AggregationIntervalUnitCV = "Year"
+
+outdf.AggregationStatisticCV = "Average"
+
+outdf.AmountUnitCV = "CFS"
+
+outdf.MaximumAmountUnitCV = "AFY"
+
+outdf.ReportYearStartMonth = "10"
+
+outdf.ReportYearTypeCV = "WaterYear"
+
+outdf.VariableCV = "Allocation"
+
+outdf.VariableSpecificCV = "Allocation All"
+
+
+# Check required fields are not null
+############################################################################
+print("Check required is not null...")
+# #Check all 'required' (not NA) columns have value (not empty). Replace blank strings by NaN, if there are any
+outdf = outdf.replace('', np.nan) #replace blank strings by NaN, if there are any
+outdf_nullMand = outdf.loc[(outdf["VariableSpecificUUID"].isnull()) | (outdf["VariableSpecificUUID"] == '') |
+                           (outdf["AggregationInterval"].isnull()) | (outdf["AggregationInterval"] == '') |
+                           (outdf["AggregationIntervalUnitCV"].isnull()) | (outdf["AggregationIntervalUnitCV"] == '') |
+                           (outdf["AggregationStatisticCV"].isnull()) | (outdf["AggregationStatisticCV"] == '') |
+                           (outdf["AmountUnitCV"].isnull()) | (outdf["AmountUnitCV"] == '') |
+                           (outdf["MaximumAmountUnitCV"].isnull()) | (outdf["MaximumAmountUnitCV"] == '') |
+                           (outdf["ReportYearStartMonth"].isnull()) | (outdf["ReportYearStartMonth"] == '') |
+                           (outdf["ReportYearTypeCV"].isnull()) | (outdf["ReportYearTypeCV"] == '') |
+                           (outdf["VariableCV"].isnull()) | (outdf["VariableCV"] == '') |
+                           (outdf["VariableSpecificCV"].isnull()) | (outdf["VariableSpecificCV"] == '')]
+
+
+# Export to new csv
+############################################################################
+print("Exporting dataframe to csv...")
+outdf.to_csv('ProcessedInputData/variables.csv', index=False)
+
+#Report missing values if need be to separate csv
+if(len(outdf_nullMand.index) > 0):
+    outdf_nullMand.to_csv('ProcessedInputData/variables_missing.csv')  # index=False,
+
+
+print("Done.")
