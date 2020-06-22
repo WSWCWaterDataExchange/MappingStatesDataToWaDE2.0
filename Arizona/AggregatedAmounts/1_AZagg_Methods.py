ef0f3a074868fa62476dccf5220a351587fbc6ca
Ryan James <rjames@wswc.utah.gov>
2020-06-22 11:25:45 -0600

    Airizona Agg Data 06/22/2020
    Airizona Agg Data 06/22/2020

---
+++ Arizona/AggregatedAmounts/1_AZagg_Methods.py
@@ -0,0 +1,81 @@
+#Date Created: 06/15/2020
+#Purpose: To create AZ agg methods use information and population dataframe for WaDE_QA 2.0.
+#Notes: 1) No input csv to read, all values are more easily hardcoded into a list here and then exported to CSV.
+
+
+# Needed Libraries
+############################################################################
+import pandas as pd
+import numpy as np
+import os
+
+
+# Inputs
+############################################################################
+print("Reading inputs...")
+workingDir = "C:/Users/rjame/Documents/WSWC Documents/MappingStatesDataToWaDE2.0/Arizona/AggregatedAmounts"
+
+os.chdir(workingDir)
+
+#WaDE columns
+columnslist = [
+    "MethodUUID",
+    "ApplicableResourceTypeCV",
+    "DataConfidenceValue",
+    "DataCoverageValue",
+    "DataQualityValueCV",
+    "MethodDescription",
+    "MethodName",
+    "MethodNEMILink",
+    "MethodTypeCV"]
+
+
+# Creating output dataframe (outdf)
+############################################################################
+print("Populating dataframe...")
+outdf = pd.DataFrame(columns=columnslist)
+outdf = outdf.append(pd.Series(), ignore_index = True)  # This approach requires a blank row to be appended into the outbound dataframe.
+
+outdf.MethodUUID = "AZ_AMAModels"
+
+outdf.ApplicableResourceTypeCV = "Groundwater"
+
+outdf.DataConfidenceValue = ""
+
+outdf.DataQualityValueCV = ""
+
+outdf.DataCoverageValue = ""
+
+outdf.MethodDescription = "Simulated water supply and demand throughout the active management area."
+
+outdf.MethodName = "AMA Aggregated Models"
+
+outdf.MethodNEMILink = "https://new.azwater.gov/hydrology/groundwater-modeling/adwr-models"
+
+outdf.MethodTypeCV = "Modeled"
+
+
+# Check required fields are not null
+############################################################################
+print("Check required is not null...")
+#Check all 'required' (not NA) columns have value (not empty). Replace blank strings by NaN, if there are any
+outdf = outdf.replace('', np.nan)
+outdf_nullMand = outdf.loc[(outdf["MethodUUID"].isnull()) | (outdf["MethodUUID"] == '') |
+                           (outdf["MethodName"].isnull()) | (outdf["MethodName"] == '') |
+                           (outdf["MethodDescription"].isnull()) | (outdf["MethodDescription"] == '') |
+                           (outdf["ApplicableResourceTypeCV"].isnull()) | (outdf["ApplicableResourceTypeCV"] == '') |
+                           (outdf["MethodTypeCV"].isnull()) | (outdf["MethodTypeCV"] == '')]
+
+
+# Export to new csv
+############################################################################
+print("Exporting dataframe to csv...")
+
+outdf.to_csv('ProcessedInputData/methods.csv', index=False)
+
+#Report missing values if need be to separate csv
+if(len(outdf_nullMand.index) > 0):
+    outdf_nullMand.to_csv('ProcessedInputData/methods_missing.csv')  # index=False,
+
+
+print("Done.")
