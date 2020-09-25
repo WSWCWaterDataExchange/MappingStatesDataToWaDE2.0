676c1c9e70970c46f63ec230ace255975c4b7ba3
Ryan James <rjames@wswc.utah.gov>
2020-09-25 15:59:11 -0600

    Utah SiteSpecific data 09252020.
    Utah SiteSpecific data 09252020.

---
+++ Utah/SiteSpecificAmounts/UDWR/3_UDWR_SS_Organizations.py
@@ -0,0 +1,82 @@
+#Last Updated: 09/08/2020
+#Purpose: To create UT site specific organization use information and population dataframe for WaDE_QA 2.0.
+#Notes: 1) No input csv to read, all values are more easily hardcoded into a list here and then exported to CSV.
+
+
+# Needed Libraries
+############################################################################
+import pandas as pd
+import numpy as np
+import os
+
+
+# Inputs
+############################################################################
+print("Reading inputs...")
+workingDir = "C:/Users/rjame/Documents/WSWC Documents/MappingStatesDataToWaDE2.0/Utah/SiteSpecificAmounts/UDWR"
+os.chdir(workingDir)
+
+# Needed WaDE columns
+columnslist = [
+    "OrganizationUUID",
+    "OrganizationContactEmail",
+    "OrganizationContactName",
+    "OrganizationDataMappingURL",
+    "OrganizationName",
+    "OrganizationPhoneNumber",
+    "OrganizationPurview",
+    "OrganizationWebsite",
+    "State"]
+
+
+# Creating output dataframe (outdf)
+############################################################################
+print("Populating dataframe...")
+
+outdf = pd.DataFrame(columns=columnslist)
+outdf = outdf.append(pd.Series(), ignore_index = True)  # This approach requires a blank row to be appended into the outbound dataframe.
+
+outdf.OrganizationUUID = "UDWR"
+
+outdf.OrganizationContactEmail = "waterrights@utah.gov"
+
+outdf.OrganizationContactName = " James Greer"
+
+outdf.OrganizationDataMappingURL = "https://github.com/WSWCWaterDataExchange/MappingStatesDataToWaDE2.0/tree/master/Utah"
+
+outdf.OrganizationName = "Utah Division of Water Rights"
+
+outdf.OrganizationPhoneNumber = "801-538-7240"
+
+outdf.OrganizationPurview = "The Utah Division of Water Rights (UDWR) publishes its water right data on-demand."
+
+outdf.OrganizationWebsite = "https://waterrights.utah.gov/"
+
+outdf.State = "UT"
+
+
+# Check required fields are not null
+############################################################################
+print("Checking required is not null...")  # Check all 'required' (not NA) columns have value (not empty).
+outdf = outdf.replace('', np.nan)  # Replace blank strings by NaN, if there are any.
+outdf_nullMand = outdf.loc[(outdf["OrganizationUUID"].isnull()) | (outdf["OrganizationUUID"] == '') |
+                           (outdf["OrganizationContactEmail"].isnull()) | (outdf["OrganizationContactEmail"] == '') |
+                           (outdf["OrganizationContactName"].isnull()) | (outdf["OrganizationContactName"] == '') |
+                           (outdf["OrganizationDataMappingURL"].isnull()) | (outdf["OrganizationDataMappingURL"] == '') |
+                           (outdf["OrganizationName"].isnull()) | (outdf["OrganizationName"] == '') |
+                           (outdf["OrganizationPhoneNumber"].isnull()) | (outdf["OrganizationPhoneNumber"] == '') |
+                           (outdf["OrganizationWebsite"].isnull()) | (outdf["OrganizationWebsite"] == '') |
+                           (outdf["State"].isnull()) | (outdf["State"] == '')]
+
+
+# Export to new csv
+############################################################################
+print("Exporting dataframe to csv...")
+# save to output
+outdf.to_csv('ProcessedInputData/organizations.csv', index=False)
+
+if(len(outdf_nullMand.index) > 0):
+    outdf_nullMand.to_csv('organizations_mandatoryFieldMissing.csv')  # index=False,
+
+
+print("Done.")
