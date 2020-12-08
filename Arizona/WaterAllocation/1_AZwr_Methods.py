6f1ce704374b8afd82b00a8116002c412d8fe00f
Ryan James <rjames@wswc.utah.gov>
2020-12-08 11:10:29 -0700

    Arizona data update.
    Arizona data update.

---
+++ Arizona/WaterAllocation/1_AZwr_Methods.py
@@ -0,0 +1,79 @@
+#Date Created: 12/01/2020
+#Purpose: To extract AZ methods use information and population dataframe for WaDE_QA 2.0.
+#Notes: 1) Two different data sets, groundwater vs surface water.
+
+
+# Needed Libraries
+############################################################################
+import pandas as pd
+import numpy as np
+import os
+
+
+# Inputs
+############################################################################
+print("Reading inputs...")
+workingDir = "C:/Users/rjame/Documents/WSWC Documents/MappingStatesDataToWaDE2.0/Arizona/WaterAllocation"
+os.chdir(workingDir)
+
+#WaDE columns
+columnslist = [
+    "MethodUUID",
+    "ApplicableResourceTypeCV",
+    "DataConfidenceValue",
+    "DataCoverageValue",
+    "DataQualityValueCV",
+    "MethodDescription",
+    "MethodName",
+    "MethodNEMILink",
+    "MethodTypeCV"]
+
+
+# Creating output dataframe (outdf)
+############################################################################
+print("Populating dataframe...")
+
+outdf = pd.DataFrame(columns=columnslist)
+
+outdf.MethodUUID = ["ADWR_Water Rights GW", "ADWR_Water Rights SW"]
+
+outdf.ApplicableResourceTypeCV = ["Groundwater", "Surface Water"]
+
+outdf.DataConfidenceValue = ""
+
+outdf.DataQualityValueCV = ""
+
+outdf.DataCoverageValue = ""
+
+outdf.MethodDescription = ["Water Rights", "Water Rights"]
+
+outdf.MethodName = ["Arizona Water Rights", "Arizona Water Rights"]
+
+outdf.MethodNEMILink = ["http://gisdata-azwater.opendata.arcgis.com/", "http://gisdata-azwater.opendata.arcgis.com/"]
+
+outdf.MethodTypeCV = ["Adjudicated", "Adjudicated"]
+
+# Check required fields are not null
+############################################################################
+print("Check required is not null...")
+#Check all 'required' (not NA) columns have value (not empty). Replace blank strings by NaN, if there are any
+outdf = outdf.replace('', np.nan)
+outdf_nullMand = outdf.loc[(outdf["MethodUUID"].isnull()) | (outdf["MethodUUID"] == '') |
+                           (outdf["MethodName"].isnull()) | (outdf["MethodName"] == '') |
+                           (outdf["MethodDescription"].isnull()) | (outdf["MethodDescription"] == '') |
+                           (outdf["ApplicableResourceTypeCV"].isnull()) | (outdf["ApplicableResourceTypeCV"] == '') |
+                           (outdf["MethodTypeCV"].isnull()) | (outdf["MethodTypeCV"] == '')]
+
+
+# Export to new csv
+############################################################################
+print("Exporting dataframe to csv...")
+
+outdf.to_csv('ProcessedInputData/methods.csv', index=False)
+
+#Report missing values if need be to separate csv
+if(len(outdf_nullMand.index) > 0):
+    outdf_nullMand.to_csv('ProcessedInputData/methods_mandatoryFieldMissing.csv')  # index=False,
+
+
+print("Done.")
