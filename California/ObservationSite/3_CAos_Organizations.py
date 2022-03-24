317bcb4865e381fdab50fe2e8d75c39b4c8cf9ea
Ryan James <rjames@wswc.utah.gov>
2022-03-24 15:59:55 -0600

    CA os update.
    CA os update.  Stream and Reservoir observation site data.

---
+++ California/ObservationSite/3_CAos_Organizations.py
@@ -0,0 +1,83 @@
+#Last Updated: 03/24/2022
+#Author: Ryan James (WSWC)
+#Purpose: To create CA site specific organization use information and population dataframe for WaDE_QA 2.0.
+#Notes: 1) No input csv to read, all values are more easily hardcoded into a list here and then exported to CSV.
+
+
+# Needed Libraries
+############################################################################
+import os
+import numpy as np
+import pandas as pd
+
+
+# Inputs
+############################################################################
+print("Reading inputs...")
+workingDir = "C:/Users/rjame/Documents/WSWC Documents/MappingStatesDataToWaDE2.0/California/ObservationSite"
+os.chdir(workingDir)
+
+# Needed WaDE columns
+columnslist = [
+    "OrganizationUUID",
+    "OrganizationContactEmail",
+    "OrganizationContactName",
+    "OrganizationDataMappingURL",
+    "OrganizationName",
+    "OrganizationPhoneNumber",
+    "OrganizationPurview",
+    "OrganizationWebsite",
+    "State"]
+
+
+# Creating output dataframe (outdf)
+############################################################################
+print("Populating dataframe...")
+outdf = pd.DataFrame(columns=columnslist)
+outdf = outdf.append(pd.Series(), ignore_index = True)  # This approach requires a blank row to be appended into the outbound dataframe.
+
+outdf.OrganizationUUID = "CAos_O1"
+
+outdf.OrganizationContactEmail = "Greg.Gearheart@waterboards.ca.gov"
+
+outdf.OrganizationContactName = "Greg Gearheart"
+
+outdf.OrganizationDataMappingURL = "https://github.com/WSWCWaterDataExchange/MappingStatesDataToWaDE2.0/tree/master/California"
+
+outdf.OrganizationName = "California State Water Resources Control Board"
+
+outdf.OrganizationPhoneNumber = "916-341-5892"
+
+outdf.OrganizationPurview = "The Electronic Water Rights Information Management System (eWRIMS) is a computer database developed by the State Water Resources Control Board to track information on water rights in California."
+
+outdf.OrganizationWebsite = "https://www.waterboards.ca.gov/waterrights/water_issues/programs/ewrims/"
+
+outdf.State = "CA"
+
+
+# Check required fields are not null
+############################################################################
+print("Checking required is not null...")  # Check all 'required' (not NA) columns have value (not empty).
+outdf = outdf.replace('', np.nan)  # Replace blank strings by NaN, if there are any.
+outdf_nullMand = outdf.loc[(outdf["OrganizationUUID"].isnull()) | (outdf["OrganizationUUID"] == '') |
+                           (outdf["OrganizationContactEmail"].isnull()) | (outdf["OrganizationContactEmail"] == '') |
+                           (outdf["OrganizationContactName"].isnull()) | (outdf["OrganizationContactName"] == '') |
+                           (outdf["OrganizationDataMappingURL"].isnull()) | (outdf["OrganizationDataMappingURL"] == '') |
+                           (outdf["OrganizationName"].isnull()) | (outdf["OrganizationName"] == '') |
+                           (outdf["OrganizationPhoneNumber"].isnull()) | (outdf["OrganizationPhoneNumber"] == '') |
+                           (outdf["OrganizationWebsite"].isnull()) | (outdf["OrganizationWebsite"] == '') |
+                           (outdf["State"].isnull()) | (outdf["State"] == '')]
+
+
+# Export to new csv
+############################################################################
+print("Exporting dataframe to csv...")
+
+# The working output DataFrame for WaDE 2.0 input.
+outdf.to_csv('ProcessedInputData/organizations.csv', index=False)
+
+#Report missing values if need be to separate csv
+if(len(outdf_nullMand.index) > 0):
+    outdf_nullMand.to_csv('organizations_mandatoryFieldMissing.csv', index=False)
+
+print("Done.")
