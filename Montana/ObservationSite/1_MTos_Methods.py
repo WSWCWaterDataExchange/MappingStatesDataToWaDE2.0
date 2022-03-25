ccf9e7c61a5e6b3c24dabdffc848646eafc36f63
Ryan James <rjames@wswc.utah.gov>
2022-03-25 10:29:51 -0600

    MT os update,
    MT os update.  Transferring MT ss data to new os data type.

---
+++ Montana/ObservationSite/1_MTos_Methods.py
@@ -0,0 +1,81 @@
+#Last Updated: 03/25/2022
+#Author: Ryan James (WSWC)
+#Purpose: To create MT observation site methods use information and population dataframe for WaDE_QA 2.0.
+#Notes: 1) Used a list approach.  Needed to have two rows, one with surface water, the other with groundwater.
+
+
+# Needed Libraries
+############################################################################
+import os
+import numpy as np
+import pandas as pd
+
+
+# Inputs
+############################################################################
+print("Reading inputs...")
+workingDir = "C:/Users/rjame/Documents/WSWC Documents/MappingStatesDataToWaDE2.0/Montana/ObservationSite"
+os.chdir(workingDir)
+
+#WaDE columns
+columnslist = [
+    "MethodUUID",
+    "ApplicableResourceTypeCV",
+    "DataConfidenceValue",
+    "DataCoverageValue",
+    "DataQualityValueCV",
+    "MethodDescription",
+    "MethodName",
+    "MethodNEMILink",
+    "MethodTypeCV"]
+
+
+# Creating output dataframe (outdf)
+############################################################################
+print("Populating dataframe...")
+outdf = pd.DataFrame(columns=columnslist)
+outdf = outdf.append(pd.Series(), ignore_index = True)  # This approach requires a blank row to be appended into the outbound dataframe.
+
+outdf.MethodUUID = "MTos_M1"
+
+outdf.ApplicableResourceTypeCV = "Surface Water"
+
+outdf.DataConfidenceValue = ""
+
+outdf.DataQualityValueCV = ""
+
+outdf.DataCoverageValue = ""
+
+outdf.MethodDescription = "Streamgage"
+
+outdf.MethodName = "Streamgage"
+
+outdf.MethodNEMILink = ""
+
+outdf.MethodTypeCV = "Measured"
+
+
+# Check required fields are not null
+############################################################################
+print("Check required is not null...")
+#Check all 'required' (not NA) columns have value (not empty). Replace blank strings by NaN, if there are any
+outdf = outdf.replace('', np.nan)
+outdf_nullMand = outdf.loc[(outdf["MethodUUID"].isnull()) | (outdf["MethodUUID"] == '') |
+                           (outdf["MethodName"].isnull()) | (outdf["MethodName"] == '') |
+                           (outdf["MethodDescription"].isnull()) | (outdf["MethodDescription"] == '') |
+                           (outdf["ApplicableResourceTypeCV"].isnull()) | (outdf["ApplicableResourceTypeCV"] == '') |
+                           (outdf["MethodTypeCV"].isnull()) | (outdf["MethodTypeCV"] == '')]
+
+
+# Export to new csv
+############################################################################
+print("Exporting dataframe to csv...")
+
+# The working output DataFrame for WaDE 2.0 input.
+outdf.to_csv('ProcessedInputData/methods.csv', index=False)
+
+#Report missing values if need be to separate csv
+if(len(outdf_nullMand.index) > 0):
+    outdf_nullMand.to_csv('ProcessedInputData/methods_missing.csv', index=False)
+
+print("Done.")
