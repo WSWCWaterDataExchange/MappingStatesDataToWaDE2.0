47f90503041fdf3ea99ab8b8ced6f197f8b2d252
Ryan James <rjames@wswc.utah.gov>
2022-03-25 14:49:11 -0600

    CO ss update.
    CO ss update.  Transferring to observation site data type.

---
+++ Colorado/ObservationSite/3_COos_Organizations.py
@@ -0,0 +1,83 @@
+#Last Updated: 03/25/2022
+#Author: Ryan James (WSWC)
+#Purpose: To create CO observation site organization use information and population dataframe for WaDE_QA 2.0.
+#Notes: 1) No input csv to read, all values are more easily hardcoded into a list here and then exported to CSV.
+
+
+# Needed Libraries
+############################################################################
+import os
+import numpy as np
+import pandas as pd
+
+
+# Inputs
+############################################################################
+print("Reading inputs...")
+workingDir = "C:/Users/rjame/Documents/WSWC Documents/MappingStatesDataToWaDE2.0/Colorado/ObservationSite"
+os.chdir(workingDir)
+
+# Needed WaDE columns
+columnslist = [
+    "OrganizationUUID",
+    "OrganizationContactEmail",
+    "OrganizationContactName",
+    "OrganizationDataMappingURL",
+    "OrganizationName",
+    "OrganizationPhoneNumber",
+    "OrganizationPurview",
+    "OrganizationWebsite",
+    "State"]
+
+
+# Creating output dataframe (outdf)
+############################################################################
+print("Populating dataframe...")
+outdf = pd.DataFrame(columns=columnslist)
+outdf = outdf.append(pd.Series(), ignore_index = True)  # This approach requires a blank row to be appended into the outbound dataframe.
+
+outdf.OrganizationUUID = "COos_O1"
+
+outdf.OrganizationContactEmail = "doug.stenzel@state.co.us"
+
+outdf.OrganizationContactName = "Doug Stenzel"
+
+outdf.OrganizationDataMappingURL = "https://github.com/WSWCWaterDataExchange/MappingStatesDataToWaDE2.0/tree/master/Colorado"
+
+outdf.OrganizationName = "Colorado Division of Water Resources"
+
+outdf.OrganizationPhoneNumber = "303-866-3581"
+
+outdf.OrganizationPurview = "Water Administration for the State of Colorado."
+
+outdf.OrganizationWebsite = "https://dwr.colorado.gov/about-us/contact-us/denver-office"
+
+outdf.State = "CO"
+
+
+# Check required fields are not null
+############################################################################
+print("Checking required is not null...")  # Check all 'required' (not NA) columns have value (not empty).
+outdf = outdf.replace('', np.nan)  # Replace blank strings by NaN, if there are any.
+outdf_nullMand = outdf.loc[(outdf["OrganizationUUID"].isnull()) | (outdf["OrganizationUUID"] == '') |
+                           (outdf["OrganizationContactEmail"].isnull()) | (outdf["OrganizationContactEmail"] == '') |
+                           (outdf["OrganizationContactName"].isnull()) | (outdf["OrganizationContactName"] == '') |
+                           (outdf["OrganizationDataMappingURL"].isnull()) | (outdf["OrganizationDataMappingURL"] == '') |
+                           (outdf["OrganizationName"].isnull()) | (outdf["OrganizationName"] == '') |
+                           (outdf["OrganizationPhoneNumber"].isnull()) | (outdf["OrganizationPhoneNumber"] == '') |
+                           (outdf["OrganizationWebsite"].isnull()) | (outdf["OrganizationWebsite"] == '') |
+                           (outdf["State"].isnull()) | (outdf["State"] == '')]
+
+
+# Export to new csv
+############################################################################
+print("Exporting dataframe to csv...")
+
+# The working output DataFrame for WaDE 2.0 input.
+outdf.to_csv('ProcessedInputData/organizations.csv', index=False)
+
+#Report missing values if need be to separate csv
+if(len(outdf_nullMand.index) > 0):
+    outdf_nullMand.to_csv('organizations_mandatoryFieldMissing.csv', index=False)
+
+print("Done.")
---
+++ Colorado/ObservationSite/4_COos_WaterSources.py
@@ -0,0 +1,133 @@
+#Last Updated: 03/25/2022
+#Author: Ryan James (WSWC)
+#Purpose: To create CO observation site water source use information and population dataframe for WaDE_QA 2.0.
+#Notes:
+
+# Needed Libraries
+############################################################################
+import os
+import numpy as np
+import pandas as pd
+
+# Custom Libraries
+############################################################################
+import sys
+sys.path.append("C:/Users/rjame/Documents/WSWC Documents/MappingStatesDataToWaDE2.0/CustomFunctions/ErrorCheckCode")
+import TestErrorFunctions
+
+
+# Inputs
+############################################################################
+print("Reading input csv...")
+workingDir = "C:/Users/rjame/Documents/WSWC Documents/MappingStatesDataToWaDE2.0/Colorado/ObservationSite"
+os.chdir(workingDir)
+fileInput = "RawinputData/P_coOSMaster.csv"
+df = pd.read_csv(fileInput)
+
+#WaDE columns
+columnslist = [
+    "WaterSourceUUID",
+    "Geometry",
+    "GNISFeatureNameCV",
+    "WaterQualityIndicatorCV",
+    "WaterSourceName",
+    "WaterSourceNativeID",
+    "WaterSourceTypeCV"]
+
+
+# Custom Site Functions
+############################################################################
+
+#WaterSourceUUID
+def assignWaterSourceUUID(colrowValue):
+    string1 = str(colrowValue)
+    outstring = "COos_WS" + string1
+    return outstring
+
+
+# Creating output dataframe (outdf)
+############################################################################
+print("Populating dataframe...")
+outdf = pd.DataFrame(index=df.index, columns=columnslist)  # The output dataframe for CSV.
+
+print("Geometry")
+outdf.Geometry = ""
+
+print("GNISFeatureNameCV")
+outdf.GNISFeatureNameCV = ""
+
+print("WaterQualityIndicatorCV")
+outdf.WaterQualityIndicatorCV = "Fresh"
+
+print("WaterSourceName")
+outdf['WaterSourceName'] = df['waterSource']
+
+print("WaterSourceNativeID")
+outdf['WaterSourceNativeID'] = df['in_WaterSourceNativeID']  # See pre-processing.
+
+print("WaterSourceTypeCV")
+outdf['WaterSourceTypeCV'] = df['in_WaterSourceTypeCV']  # See pre-processing.
+
+
+##############################
+# Dropping duplicate
+print("Dropping duplicates")
+outdf = outdf.drop_duplicates(subset=['WaterSourceName', 'WaterSourceNativeID', 'WaterSourceTypeCV']).reset_index(drop=True)
+##############################
+
+print("WaterSourceUUID")
+df["Count"] = range(1, len(df.index) + 1)
+outdf['WaterSourceUUID'] = df.apply(lambda row: assignWaterSourceUUID(row['Count']), axis=1)
+
+print("Resetting Index")
+outdf.reset_index()
+
+
+#Error Checking each Field
+############################################################################
+print("Error checking each field.  Purging bad inputs.")
+purgecolumnslist = ["ReasonRemoved", "RowIndex", "IncompleteField_1", "IncompleteField_2"]
+dfpurge = pd.DataFrame(columns=purgecolumnslist) # Purge DataFrame to hold removed elements
+
+# Geometry
+# ???? How to check for geometry datatype
+
+# GNISFeatureNameCV
+outdf, dfpurge = TestErrorFunctions.GNISFeatureNameCV_WS_Check(outdf, dfpurge)
+
+# WaterQualityIndicatorCV
+outdf, dfpurge = TestErrorFunctions.WaterQualityIndicatorCV_WS_Check(outdf, dfpurge)
+
+# WaterSourceName
+outdf, dfpurge = TestErrorFunctions.WaterSourceName_WS_Check(outdf, dfpurge)
+
+# WaterSourceNativeID
+outdf, dfpurge = TestErrorFunctions.WaterSourceNativeID_WS_Check(outdf, dfpurge)
+
+# WaterSourceTypeCV
+outdf, dfpurge = TestErrorFunctions.WaterSourceTypeCV_WS_Check(outdf, dfpurge)
+
+
+############################################################################
+print("Assign WaterSourceUUID") # has to be one of the last.
+outdf = outdf.reset_index(drop=True)
+dftemp = pd.DataFrame(index=outdf.index)
+dftemp["Count"] = range(1, len(dftemp.index) + 1)
+outdf['WaterSourceUUID'] = dftemp.apply(lambda row: assignWaterSourceUUID(row['Count']), axis=1)
+
+# Error check WaterSourceUUID
+outdf, dfpurge = TestErrorFunctions.WaterSourceUUID_WS_Check(outdf, dfpurge)
+
+
+# Export to new csv
+############################################################################
+print("Exporting dataframe outdf to csv...")
+
+# The working output DataFrame for WaDE 2.0 input.
+outdf.to_csv('ProcessedInputData/watersources.csv', index=False)
+
+# Report purged values.
+if(len(dfpurge.index) > 0):
+    dfpurge.to_excel('ProcessedInputData/watersources_missing.xlsx', index=False)
+
+print("Done.")
