820daaecd04241a5709def6d4eded7453fbacaa8
Ryan James <rjames@wswc.utah.gov>
2022-04-04 11:29:42 -0600

    OR wr update.
    OR wr update.  New m-m relationship with watersources to sites.  new groupby statement for allocations.  Added geometry for POU sites.

--- Oregon/WaterAllocation/4_ORwr_WaterSources.py
+++ Oregon/WaterAllocation/4_ORwr_WaterSources.py
@@ -75,10 +75,6 @@ print("Dropping duplicates")
 outdf = outdf.drop_duplicates(subset=['WaterSourceName', 'WaterSourceNativeID', 'WaterSourceTypeCV']).reset_index(drop=True)
 ##############################
 
-print("WaterSourceUUID")
-df["Count"] = range(1, len(df.index) + 1)
-outdf['WaterSourceUUID'] = df.apply(lambda row: assignWaterSourceUUID(row['Count']), axis=1)
-
 print("Resetting Index")
 outdf.reset_index()
 
