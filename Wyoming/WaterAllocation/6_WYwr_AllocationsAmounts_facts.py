5f9865af21b23c73a5cdabc050c0470d0410963c
Ryan James <rjames@wswc.utah.gov>
2022-08-23 09:56:28 -0600

    WY wr update.
    WY wr update.  Removed special characters from owner info.  Added more data assessment logic.

--- Wyoming/WaterAllocation/6_WYwr_AllocationsAmounts_facts.py
+++ Wyoming/WaterAllocation/6_WYwr_AllocationsAmounts_facts.py
@@ -1,4 +1,4 @@
-#Date Created: 03/24/2022
+#Date Updated: 08/01/2022
 #Purpose: To extract WY water right information and populate dataframe for WaDE_QA 2.0.
 #         1) Simple creation of working dataframe (df), with output dataframe (outdf).
 #         2) Drop all nulls before combining duplicate rows on NativeID.
@@ -31,6 +31,8 @@ df_sites = pd.read_csv(sites_fileInput)  # Sites dataframe
 
 #WaDE dataframe columns
 columnslist = [
+    "WaDEUUID",
+    "WaDEUUID",
     "AllocationUUID",
     "MethodUUID",
     "OrganizationUUID",
@@ -231,6 +233,12 @@ outdf['PrimaryBeneficialUseCategory'] = "Unspecified"
 print("WaterAllocationNativeURL")
 outdf['WaterAllocationNativeURL'] = ""
 
+print("Adding Data Assessment UUID")
+outdf['WaDEUUID'] = df_DM['WaDEUUID']
+
+print("Adding Data Assessment UUID")
+outdf['WaDEUUID'] = ""
+
 print("Resetting Index")
 outdf.reset_index()
 
@@ -245,10 +253,14 @@ outdf = outdf[columnslist]  # reorder the dataframe's columns based on columnsli
 # ############################################################################
 print("Solving WaDE 2.0 upload issues")  # List all temp fixes required to upload data to QA here.
 
-# Date Noted: 05/25/2021
 # Note: OwnerClassificationCV can only accept 1 entry at this time. Error due to above merge / we don't allow multiple OwnerClassificationCV.
-def tempfixOCSV(colrowValueA):
-    result = colrowValueA.split(",", 1)[0]  # pass in text, split on "," & return first value.
+def tempfixOCSV(val):
+    valList = val.split(",") # convert string to list
+    valList.sort() # sort list alphabetically
+    if ("In Review" in valList):
+        valList.remove("In Review") # check if "In Review"  If true, remove.
+        valList.append("In Review") # Append back in "In Review" to end of list.
+    result = valList[0] # return only first value in list.
     return result
 outdf['OwnerClassificationCV']  = outdf.apply(lambda row: tempfixOCSV(row['OwnerClassificationCV']), axis=1)
 
@@ -262,7 +274,7 @@ outdf['PrimaryBeneficialUseCategory'] = outdf.apply(lambda row: AssignPrimaryUse
 #Error checking each field
 ############################################################################
 print("Error checking each field.  Purging bad inputs.")
-purgecolumnslist = ["ReasonRemoved", "RowIndex", "IncompleteField_1", "IncompleteField_2"]
+purgecolumnslist = ["ReasonRemoved", "WaDEUUID", "RowIndex", "IncompleteField_1", "IncompleteField_2"]
 dfpurge = pd.DataFrame(columns=purgecolumnslist) # Purge DataFrame to hold removed elements
 
 # MethodUUID
@@ -386,6 +398,12 @@ outdf['AllocationUUID'] = dftemp.apply(lambda row: assignAllocationUUID(row['Cou
 
 # Error check AllocationUUID
 outdf, dfpurge = TestErrorFunctions.AllocationUUID_AA_Check(outdf, dfpurge)
+
+
+# Remove WaDEUUID field from import file (only needed for purge info).
+############################################################################
+print("Drop Assessment WaDEUUID")
+outdf = outdf.drop(['WaDEUUID'], axis=1)
 
 
 # Export to new csv
---
+++ Wyoming/WaterAllocation/DataAssessment/ReadME.md
@@ -0,0 +1,7 @@
+# Data Review
+The following is a set notes that the Water Data Exchange (WaDE) staff has assembled of how well the provided data fit into the WaDE 2.0 architecture.  These notes relate to unidentifiable elements and irregular entries, missing WaDE Key elements, and insights of what improvements could be made to the provided datasets.
+
+
+## Status
+- See **NV ss Data Assessment Report 2022-02-03.docx** for notes.
+- See **Incomplete Records.xlsx** for a complete list of records that could not be uploaded into WaDE.
