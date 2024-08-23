3338f80b5209568f11ae6831516e682ca77e1077
Ryan James <rjames@wswc.utah.gov>
2024-08-23 11:44:33 -0600

    CAag update
    changed file name to WaterUse_Aggregated

---
+++ California/WaterUse_Aggregated/4_CAag_WaterSources.py
@@ -0,0 +1,129 @@
+# Date Created: 04/04/2022
+#Author: Ryan James (WSWC)
+#Purpose: To create CA agg water source use information and populate a dataframe for WaDE_QA 2.0.
+#Notes: N/A
+
+
+# Needed Libraries
+############################################################################
+import os
+import numpy as np
+import pandas as pd
+
+# Custom Libraries
+############################################################################
+import sys
+sys.path.append("C:/Users/rjame/Documents/WSWC Documents/MappingStatesDataToWaDE2.0/5_CustomFunctions/ErrorCheckCode")
+import TestErrorFunctions
+
+
+# Inputs
+############################################################################
+print("Reading input csv...")
+workingDir = "G:/Shared drives/WaDE Data/California/AggregatedAmounts"
+os.chdir(workingDir)
+fileInput = "RawinputData/P_caAggMaster.csv"
+df = pd.read_csv(fileInput).replace(np.nan, "")  # The State's Master input dataframe. Remove any nulls.
+
+#WaDE columns
+columnslist = [
+    "WaterSourceUUID",
+    "Geometry",
+    "GNISFeatureNameCV",
+    "WaterQualityIndicatorCV",
+    "WaterSourceName",
+    "WaterSourceNativeID",
+    "WaterSourceTypeCV"]
+
+
+# Custom Site Functions
+############################################################################
+
+# For creating WaDESiteUUID
+def assignWaterSourceUUID(colrowValue):
+    string1 = str(colrowValue)
+    outstring = "CAag_WS" + string1
+    return outstring
+
+
+# Creating output dataframe (outdf)
+############################################################################
+print("Populating dataframe...")
+outdf = pd.DataFrame(index=df.index, columns=columnslist)  # The output dataframe for CSV.
+
+print("Geometry")
+outdf['Geometry'] = ""
+
+print("GNISFeatureNameCV")
+outdf['GNISFeatureNameCV'] = ""
+
+print("WaterQualityIndicatorCV")
+outdf['WaterQualityIndicatorCV'] = "Fresh"
+
+print("WaterSourceName")
+outdf['WaterSourceName'] = "Unspecified"
+
+print("WaterSourceNativeID")
+outdf['WaterSourceNativeID'] = "WaDECA_WS1"
+
+print("WaterSourceTypeCV")
+outdf['WaterSourceTypeCV'] = 'Surface and Groundwater'
+
+##############################
+# Dropping duplicate
+print("Dropping duplicates")
+outdf = outdf.drop_duplicates(subset=['WaterSourceName', 'WaterSourceNativeID', 'WaterSourceTypeCV']).reset_index(drop=True)
+##############################
+
+print("Resetting Index")
+outdf.reset_index()
+
+
+#Error Checking each Field
+############################################################################
+print("Error checking each field.  Purging bad inputs.")
+purgecolumnslist = ["ReasonRemoved", "RowIndex", "IncompleteField_1", "IncompleteField_2"]
+dfpurge = pd.DataFrame(columns=purgecolumnslist) # Purge DataFrame to hold removed elements
+
+# Geometry
+# ???? How to check for geometry datatype
+
+# GNISFeatureNameCV
+outdf, dfpurge = TestErrorFunctions.GNISFeatureNameCV_WS_Check(outdf, dfpurge)
+
+# WaterQualityIndicatorCV
+outdf, dfpurge = TestErrorFunctions.WaterQualityIndicatorCV_WS_Check(outdf, dfpurge)
+
+# WaterSourceName
+outdf, dfpurge = TestErrorFunctions.WaterSourceName_WS_Check(outdf, dfpurge)
+
+# WaterSourceNativeID
+outdf, dfpurge = TestErrorFunctions.WaterSourceNativeID_WS_Check(outdf, dfpurge)
+
+# WaterSourceTypeCV
+outdf, dfpurge = TestErrorFunctions.WaterSourceTypeCV_WS_Check(outdf, dfpurge)
+
+
+############################################################################
+print("Assign WaterSourceUUID") # has to be one of the last.
+outdf = outdf.reset_index(drop=True)
+dftemp = pd.DataFrame(index=outdf.index)
+dftemp["Count"] = range(1, len(dftemp.index) + 1)
+outdf['WaterSourceUUID'] = dftemp.apply(lambda row: assignWaterSourceUUID(row['Count']), axis=1)
+
+# Error check WaterSourceUUID
+outdf, dfpurge = TestErrorFunctions.WaterSourceUUID_WS_Check(outdf, dfpurge)
+
+
+# Export to new csv
+############################################################################
+print("Exporting outdf and dfpurge dataframes...")
+
+# The working output DataFrame for WaDE 2.0 input.
+outdf.to_csv('ProcessedInputData/watersources.csv', index=False)
+
+# Report purged values.
+if(len(dfpurge.index) > 0):
+    dfpurge.to_excel('ProcessedInputData/watersources_missing.xlsx', index=False)
+
+print("Done.")
---
+++ California/WaterUse_Aggregated/5_CAag_ReportingUnits.py
@@ -0,0 +1,162 @@
+# Date Created: 04/04/2022
+# Author: Ryan James (WSWC)
+# Purpose: To create CA agg reporting unit information and populate a dataframe for WaDEQA 2.0.
+# Notes: N/A
+
+
+# Needed Libraries
+############################################################################
+import os
+import numpy as np
+import pandas as pd
+
+# Custom Libraries
+############################################################################
+import sys
+sys.path.append("C:/Users/rjame/Documents/WSWC Documents/MappingStatesDataToWaDE2.0/5_CustomFunctions/ErrorCheckCode")
+import TestErrorFunctions
+
+
+# Inputs
+############################################################################
+print("Reading input csv...")
+workingDir = "G:/Shared drives/WaDE Data/California/AggregatedAmounts"
+os.chdir(workingDir)
+fileInput = "RawinputData/P_caAggMaster.csv"
+df = pd.read_csv(fileInput).replace(np.nan, "")  # The State's Master input dataframe. Remove any nulls.
+fileInput_shape = "RawinputData/P_caGeometry.csv"
+dfshape = pd.read_csv(fileInput_shape)
+
+columnslist =[
+    "ReportingUnitUUID",
+    "EPSGCodeCV",
+    "ReportingUnitName",
+    "ReportingUnitNativeID",
+    "ReportingUnitProductVersion",
+    "ReportingUnitTypeCV",
+    "ReportingUnitUpdateDate",
+    "StateCV",
+    "Geometry"]
+
+
+# Custom Functions
+############################################################################
+
+# For Creating Geometry
+Geometrydict = pd.Series(dfshape.geometry.values, index = dfshape.RU_ID).to_dict()
+def retrieveGeometry(colrowValue):
+    if colrowValue == '' or pd.isnull(colrowValue):
+        outList = ''
+    else:
+        String1 = colrowValue.strip()
+        try:
+            outList = Geometrydict[String1]
+        except:
+            outList = ''
+    return outList
+
+
+# For creating SiteUUID
+def assignReportingUnitID(colrowValue):
+    string1 = str(colrowValue)
+    outstring = "CAag_RU" + string1
+    return outstring
+
+
+# Creating output dataframe (outdf)
+############################################################################
+print("Populating dataframe...")
+outdf = pd.DataFrame(columns=columnslist, index=df.index)
+
+print("EPSGCodeCV")
+outdf['EPSGCodeCV'] = "4326"
+
+print("ReportingUnitName")
+outdf['ReportingUnitName'] = df['in_ReportingUnitName']
+
+print("ReportingUnitNativeID")
+outdf['ReportingUnitNativeID'] = df['in_ReportingUnitNativeID']
+
+print("ReportingUnitProductVersion")
+outdf['ReportingUnitProductVersion'] = ""
+
+print("ReportingUnitTypeCV")
+outdf['ReportingUnitTypeCV'] = df['in_ReportingUnitTypeCV']
+
+print("ReportingUnitUpdateDate")
+outdf['ReportingUnitUpdateDate'] = ""
+
+print("StateCV")
+outdf['StateCV'] = "CA"
+
+print("Geometry")
+outdf['Geometry'] = df.apply(lambda row: retrieveGeometry(row['in_ReportingUnitNativeID']), axis=1)
+
+print("Resetting Index")
+outdf.reset_index()
+
+#####################################
+# Dropping duplicate
+# filter the whole table based on a unique combination of ReportingUnitName, ReportingUnitNativeID & ReportingUnitTypeCV
+outdf = outdf.drop_duplicates(subset=['ReportingUnitName', 'ReportingUnitNativeID', 'ReportingUnitTypeCV'])
+outdf = outdf.reset_index(drop=True)
+######################################
+
+print("Resetting Index")
+outdf.reset_index()
+
+
+#Error Checking each Field
+############################################################################
+print("Error checking each field.  Purging bad inputs.")
+purgecolumnslist = ["ReasonRemoved", "RowIndex", "IncompleteField_1", "IncompleteField_2"]
+dfpurge = pd.DataFrame(columns=purgecolumnslist) # Purge DataFrame to hold removed elements
+
+# EPSGCodeCV
+outdf, dfpurge = TestErrorFunctions.EPSGCodeCV_RU_Check(outdf, dfpurge)
+
+# ReportingUnitName
+outdf, dfpurge = TestErrorFunctions.ReportingUnitName_RU_Check(outdf, dfpurge)
+
+# ReportingUnitNativeID
+outdf, dfpurge = TestErrorFunctions.ReportingUnitNativeID_RU_Check(outdf, dfpurge)
+
+# ReportingUnitProductVersion
+outdf, dfpurge = TestErrorFunctions.ReportingUnitProductVersion_RU_Check(outdf, dfpurge)
+
+# ReportingUnitTypeCV
+outdf, dfpurge = TestErrorFunctions.ReportingUnitTypeCV_RU_Check(outdf, dfpurge)
+
+# ReportingUnitUpdateDate
+outdf, dfpurge = TestErrorFunctions.ReportingUnitUpdateDate_RU_Check(outdf, dfpurge)
+
+# StateCV
+outdf, dfpurge = TestErrorFunctions.StateCV_RU_Check(outdf, dfpurge)
+
+# Geometry
+# ???? How to check for geometry datatype
+
+
+############################################################################
+print("Assign ReportingUnitUUID") # has to be one of the last.
+outdf = outdf.reset_index(drop=True)
+dftemp = pd.DataFrame(index=outdf.index)
+dftemp["Count"] = range(1, len(dftemp.index) + 1)
+outdf['ReportingUnitUUID'] = dftemp.apply(lambda row: assignReportingUnitID(row['Count']), axis=1)
+
+# Error Check ReportingUnitUUID
+outdf, dfpurge = TestErrorFunctions.ReportingUnitUUID_RU_Check(outdf, dfpurge)
+
+
+# Export to new csv
+############################################################################
+print("Exporting outdf and dfpurge dataframes...")
+
+# The working output DataFrame for WaDE 2.0 input.
+outdf.to_csv('ProcessedInputData/reportingunits.csv', index=False)
+
+# Report purged values.
+if(len(dfpurge.index) > 0):
+    dfpurge.to_excel('ProcessedInputData/reportingunits_missing.xlsx', index=False)
+
+print("Done.")
